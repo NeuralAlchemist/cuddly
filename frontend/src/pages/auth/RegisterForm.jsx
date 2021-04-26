42d2c73963dd0df850db762c9fa6d1b26bbf6e2c
Yash Seeta <yashseeta94@gmail.com>
2021-04-26 14:03:31 +0200

    remove bootstrap classnames

--- frontend/src/pages/auth/RegisterForm.jsx
+++ frontend/src/pages/auth/RegisterForm.jsx
@@ -1,51 +1,49 @@
+// NPM Packages
 import React, { useState } from "react";
 
 export default function RegisterForm({ onSubmit }) {
+  // Local States
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <div className="card">
-      <div className="card-body">
-        <h4 className="card-title">Sign up</h4>
+    <div>
+      <div >
+        <h4 >Sign up</h4>
         <div>
-          <div className="form-group">
+          <div >
             <label>Name:</label>
             <input
               type="text"
-              className="form-control"
               value={name}
               onChange={(e) => setName(e.target.value)}
               placeholder="Name"
             />
           </div>
 
-          <div className="form-group">
+          <div>
             <label>Email:</label>
             <input
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
               placeholder="Email"
             />
           </div>
 
-          <div className="form-group">
+          <div>
             <label>Password:</label>
             <input
               type="password"
               placeholder="Password"
-              className="form-control"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
-          <div className="form-group">
+          <div>
             <button
-              className="btn btn-success"
               onClick={(e) => onSubmit({ name, email, password })}
             >
               Create account
