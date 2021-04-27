2f3564fe64696f9c3605aa5197c47a07e8a9177c
En-Chi Liu <enchi.liu@gmail.com>
2021-04-27 13:17:51 +0200

    Coded CommentCard.jsx

---
+++ frontend/src/pages/posts/comments/CommentCard.jsx
@@ -0,0 +1,20 @@
+// NPM Packages
+import React from "react";
+
+export default function Comment({ comment, onDeleteClick }) {
+    // Methods
+    const handleDelete = () => {
+        // Invoke the passed in event callback
+        onDeleteClick(comment);
+    };
+
+    return (
+        <div>
+            <p>{comment.contentText}</p>
+
+            <button onClick={handleDelete}>
+                    Delete
+            </button>
+        </div>
+    );
+}
