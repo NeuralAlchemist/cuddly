7950b2d63a576fa5615cb8ed492526cfcaa5b1c3
Janko Radakovic <mailodjanka@gmail.com>
2021-04-27 09:20:46 +0200

    Adds notblank to Post model bady and id as json identity

--- src/main/java/se/kth/sda/skeleton/posts/Post.java
+++ src/main/java/se/kth/sda/skeleton/posts/Post.java
@@ -1,17 +1,21 @@
 package se.kth.sda.skeleton.posts;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Column(nullable = false)
     private String body;
 
     public Post() {
