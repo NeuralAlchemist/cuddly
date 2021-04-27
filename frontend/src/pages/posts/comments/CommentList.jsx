b49aecea5ecf1eb5c4098b868d79f903a425cd5e
En-Chi Liu <enchi.liu@gmail.com>
2021-04-27 13:19:50 +0200

    Coded CommentList.jsx

---
+++ frontend/src/pages/posts/comments/CommentList.jsx
@@ -0,0 +1,19 @@
+// NPM Packages
+import React from "react";
+
+// Project files
+import CommentCard from "./CommentCard";
+
+export default function CommentList({ comments, onDelete }) {
+    // Components
+    const CommentCardArray = comments.map((comment) => (
+        <CommentCard key={comment.id} comment={comment} onDeleteClick={onDelete} />
+    ));
+
+    return (
+        <div className="CommentList">
+            {CommentCardArray}
+        </div>
+    )
+
+}
