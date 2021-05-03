941cfdb251c6559c5666cf4f65f3fe6a4a889db5
Yash Seeta <yashseeta94@gmail.com>
2021-05-03 17:36:17 +0200

    add method to upload contentFile and contentText

--- src/main/java/se/kth/sda/skeleton/posts/PostService.java
+++ src/main/java/se/kth/sda/skeleton/posts/PostService.java
@@ -2,12 +2,14 @@ package se.kth.sda.skeleton.posts;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import se.kth.sda.skeleton.auth.AuthService;
 import se.kth.sda.skeleton.exception.ForbiddenException;
 import se.kth.sda.skeleton.exception.ResourceNotFoundException;
 import se.kth.sda.skeleton.user.User;
 import se.kth.sda.skeleton.user.UserRepository;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
@@ -76,6 +78,37 @@ public class PostService {
         return post;
     }
 
+    /**
+     * Create a post
+     *
+     * @param text the contentText of the post
+     * @param file the contentFile to be added to the post
+     * @return newly created post
+     */
+    public Post createPostImage(String text, MultipartFile file){
+        Post newPost = new Post();
+        String email = authService.getLoggedInUserEmail();
+        LocalDateTime createdTime = LocalDateTime.now();
+        User relatedUser = userRepository.findByEmail(email);
+        newPost.setContentText(text);
+        newPost.setRelatedPostUser(relatedUser);
+        newPost.setCreatedTime(createdTime);
+
+        if(!file.isEmpty()){
+            try {
+                Byte[] bytes = new Byte[file.getBytes().length];
+                int i = 0;
+                for (byte b : file.getBytes()){
+                    bytes[i++] = b;
+                }
+                newPost.setImage(bytes);
+                newPost = postRepository.save(newPost);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return postRepository.save(newPost);
+    }
     /**
      * Update a post based on ID
      *
