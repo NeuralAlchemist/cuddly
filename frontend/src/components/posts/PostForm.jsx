d48cf0966c7c94a6a1ff7c20646a5b8d6f49b25f
Yash Seeta <yashseeta94@gmail.com>
2021-05-04 17:01:44 +0200

    add logic to obtain contentText and contentFile

--- frontend/src/components/posts/PostForm.jsx
+++ frontend/src/components/posts/PostForm.jsx
@@ -2,10 +2,11 @@
 import React, { useState } from "react";
 import '../posts/postform.css';
 
-export default function PostForm({ onSubmit }) {
+export default function PostForm({ onSubmit, onImagePostSubmit }) {
   // Local State
   const [contentText, setContentText] = useState("");
-
+  const [contentFile, setContentFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
   // Methods
   const handleSubmit = () => {
     // Invoke the passed in event callback
@@ -15,6 +16,30 @@ export default function PostForm({ onSubmit }) {
     setContentText("");
   };
 
+  const handleImagePostSubmit = () => {
+    const formData = new FormData();
+    formData.append('image',contentFile);
+    onImagePostSubmit({formData,contentText});
+    setContentFile(null);
+    setContentText("");
+    setIsFilePicked(false);
+  }
+  const setFile = (event) => {
+    setContentFile(event.target.files[0]);
+    setIsFilePicked(true);
+    const formData = new FormData();
+    formData.append('file', contentFile);
+    formData.append('contentText', contentText);
+    formData.forEach(item => console.log(item))
+    console.log(formData);
+    console.log(formData.contentText)
+    console.log(`selected file is now: ${event.target.files[0]}`);
+  }
+
+  const handleUpload = () => {
+    console.log(`make a call to upload endpoint with selected file`);
+  }
+
   return (
     <form className="postform">
       <div>
@@ -23,9 +48,11 @@ export default function PostForm({ onSubmit }) {
           <div>
             <textarea value={contentText} onChange={(e) => setContentText(e.target.value)} />
           </div>
+          <input type="file" onChange={setFile}/>
           <div>
             <button onClick={handleSubmit}>Post</button>
           </div>
+          <button onClick={handleImagePostSubmit}>Upload Image</button>
         </div>
       </div>
     </form>
