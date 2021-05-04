444b3ec8bb695979b4ee2355a1f3bcae901f8219
Yash Seeta <yashseeta94@gmail.com>
2021-05-04 17:03:55 +0200

    trying out different things

--- src/main/java/se/kth/sda/skeleton/posts/ContentFile.java
+++ src/main/java/se/kth/sda/skeleton/posts/ContentFile.java
@@ -1,6 +1,34 @@
 package se.kth.sda.skeleton.posts;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class ContentFile {
     private String base64String;
     private String fileType;
+
+    public ContentFile(String base64String, String fileType) {
+        this.base64String = base64String;
+        this.fileType = fileType;
+    }
+
+    public ContentFile() {
+
+    }
+
+    public String getBase64String() {
+        return base64String;
+    }
+
+    public void setBase64String(String base64String) {
+        this.base64String = base64String;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
 }
