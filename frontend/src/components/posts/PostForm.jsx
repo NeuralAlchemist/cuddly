cb612d4f5718f5b9aeac3f7afec8e90e7ae2126b
Yash Seeta <yashseeta94@gmail.com>
2021-05-04 17:01:44 +0200

    add logic to obtain contentText and contentFile

--- frontend/src/components/posts/PostForm.jsx
+++ frontend/src/components/posts/PostForm.jsx
@@ -45,10 +45,28 @@ export default function PostForm({ onSubmit, onImagePostSubmit }) {
         });
   };
 
-  const setFile = async (event) => {
+  const handleImagePostSubmit = () => {
+    const formData = new FormData();
+    formData.append('image',contentFile);
+    onImagePostSubmit({formData,contentText});
+    setContentFile(null);
+    setContentText("");
+    setIsFilePicked(false);
+  }
+  const setFile = (event) => {
     setContentFile(event.target.files[0]);
-
+    setIsFilePicked(true);
+    const formData = new FormData();
+    formData.append('file', contentFile);
+    formData.append('contentText', contentText);
+    formData.forEach(item => console.log(item))
+    console.log(formData);
+    console.log(formData.contentText)
     console.log(`selected file is now: ${event.target.files[0]}`);
+  }
+
+  const handleUpload = () => {
+    console.log(`make a call to upload endpoint with selected file`);
   }
 
   return (
