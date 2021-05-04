ff818eb3888f6f74bfdd8470af64b7fa411e8c68
Yash Seeta <yashseeta94@gmail.com>
2021-05-04 17:01:44 +0200

    add logic to obtain contentText and contentFile

--- frontend/src/components/posts/PostForm.jsx
+++ frontend/src/components/posts/PostForm.jsx
@@ -1,6 +1,7 @@
 // NPM Packages
 import React, { useState } from 'react';
 
+<<<<<<< HEAD
 // Project Files
 import ResponsiveTextArea from '../ResponsiveTextArea';
 import FormFooter from "../FormFooter";
@@ -10,6 +11,14 @@ export default function PostForm({ onSubmit, onSubmitMedia }) {
   // Local State
   const [contentText, setContentText] = useState("");
   const [length, setLength] = useState();
+=======
+export default function PostForm({ onSubmit, onImagePostSubmit }) {
+  // Local State
+
+  const [contentText, setContentText] = useState('');
+  const postObject = require('../../assets/images/post.svg');
+  const postURL = postObject;
+>>>>>>> 2c12a9d (add logic to obtain contentText and contentFile)
   const [contentFile, setContentFile] = useState();
   const [isFilePicked, setIsFilePicked] = useState(false);
 
@@ -33,6 +42,7 @@ export default function PostForm({ onSubmit, onSubmitMedia }) {
     setLength(value.length);
   };
 
+<<<<<<< HEAD
   const setFile = (event) => {
     setContentFile(event.target.files[0]);
     setIsFilePicked(true);
@@ -49,6 +59,52 @@ export default function PostForm({ onSubmit, onSubmitMedia }) {
             onFormContentChange={onFormContentChange}
             maxLength="1000"
           />
+=======
+  const handleImagePostSubmit = () => {
+    const formData = new FormData();
+    formData.append('image',contentFile);
+    onImagePostSubmit({formData,contentText});
+    setContentFile(null);
+    setContentText("");
+    setIsFilePicked(false);
+  }
+  const setFile = (event) => {
+    setContentFile(event.target.files[0]);
+    setIsFilePicked(true);
+    const formData = new FormData();
+    formData.append('file', contentFile);
+    formData.append('contentText', contentText);
+    formData.forEach(item => console.log(item))
+    console.log(formData);
+    console.log(formData.contentText)
+    console.log(`selected file is now: ${event.target.files[0]}`);
+  }
+
+  const handleUpload = () => {
+    console.log(`make a call to upload endpoint with selected file`);
+  }
+
+  return (
+    <div className="postform-container">
+      <form className="postform">
+        <div>
+          <div>
+            <div className="postform-field">
+              <textarea 
+                className="postform-input"
+                placeholder="What's on your mind?"
+                value={contentText}
+                onChange={(e) => setContentText(e.target.value)}
+              />
+            </div>
+            <input type="file" onChange={setFile}/>
+            <button className="button-post" onClick={handleSubmit}>
+              <img className="post" src={postURL} alt="Post" />
+              <span>Post</span>
+            </button>
+          </div>
+          <button onClick={handleImagePostSubmit}>Upload Image</button>
+>>>>>>> 2c12a9d (add logic to obtain contentText and contentFile)
         </div>
         <FormFooter
           isFilePicked={isFilePicked}
