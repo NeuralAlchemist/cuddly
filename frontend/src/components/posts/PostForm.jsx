2c12a9d1e3960fec756ac66e9609e30be58410f2
Yash Seeta <yashseeta94@gmail.com>
2021-05-04 17:01:44 +0200

    add logic to obtain contentText and contentFile

--- frontend/src/components/posts/PostForm.jsx
+++ frontend/src/components/posts/PostForm.jsx
@@ -1,11 +1,14 @@
 // NPM Packages
 import React, { useState } from 'react';
 
-export default function PostForm({ onSubmit }) {
+export default function PostForm({ onSubmit, onImagePostSubmit }) {
   // Local State
+
   const [contentText, setContentText] = useState('');
   const postObject = require('../../assets/images/post.svg');
   const postURL = postObject;
+  const [contentFile, setContentFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
 
   // Methods
   const handleSubmit = () => {
@@ -15,6 +18,30 @@ export default function PostForm({ onSubmit }) {
     // Clear the input field
     setContentText('');
   };
+
+  const handleImagePostSubmit = () => {
+    const formData = new FormData();
+    formData.append('image',contentFile);
+    onImagePostSubmit({formData,contentText});
+    setContentFile(null);
+    setContentText("");
+    setIsFilePicked(false);
+  }
+  const setFile = (event) => {
+    setContentFile(event.target.files[0]);
+    setIsFilePicked(true);
+    const formData = new FormData();
+    formData.append('file', contentFile);
+    formData.append('contentText', contentText);
+    formData.forEach(item => console.log(item))
+    console.log(formData);
+    console.log(formData.contentText)
+    console.log(`selected file is now: ${event.target.files[0]}`);
+  }
+
+  const handleUpload = () => {
+    console.log(`make a call to upload endpoint with selected file`);
+  }
 
   return (
     <div className="postform-container">
@@ -29,12 +56,13 @@ export default function PostForm({ onSubmit }) {
                 onChange={(e) => setContentText(e.target.value)}
               />
             </div>
-
+            <input type="file" onChange={setFile}/>
             <button className="button-post" onClick={handleSubmit}>
               <img className="post" src={postURL} alt="Post" />
               <span>Post</span>
             </button>
           </div>
+          <button onClick={handleImagePostSubmit}>Upload Image</button>
         </div>
       </form>
     </div>
