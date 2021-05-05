8d62779873041d59df264c487539c866a048114d
Yash Seeta <yashseeta94@gmail.com>
2021-05-05 16:30:54 +0200

    add icons for edit and delete inside postcard header flex

--- frontend/src/components/posts/PostCard.jsx
+++ frontend/src/components/posts/PostCard.jsx
@@ -12,143 +12,175 @@ import PostUpdateForm from "./PostUpdateForm";
 import PostLikeApi from "../../api/PostLikeApi";
 
 export default function PostCard({ post, onDeleteClick }) {
-  // Local state
-  const [comments, setComments] = useState([]);
-  const [toggleUpdatePost, setToggleUpdatePost] = useState(false);
-  const [currentUser, setCurrentUser] = useState({});
+    // Local state
+    const [comments, setComments] = useState([]);
+    const [toggleUpdatePost, setToggleUpdatePost] = useState(false);
+    const [currentUser, setCurrentUser] = useState({});
 
-  // Constants
-  const postId = post.id;
-  const postCreatorName = post.relatedPostUser.name;
-  const postCreatorEmail = post.relatedPostUser.email;
-  const listOfLikedUsers = post.listOfLikes.map((like) => like.likedUser);
-  const likeIconURL = require('../../assets/images/like.svg');
+    // Constants
+    const deleteURL = require("../../assets/images/delete.svg");
+    const editURL = require("../../assets/images/edit.svg");
+    const postId = post.id;
+    const postCreatorName = post.relatedPostUser.name;
+    const postCreatorEmail = post.relatedPostUser.email;
+    const listOfLikedUsers = post.listOfLikes.map((like) => like.likedUser);
+    const likeIconURL = require("../../assets/images/like.svg");
 
-  // Methods
-  async function updatePost(updatedPost) {
-    try {
-      await PostsApi.updatePost(updatedPost, post.id);
-    } catch (e) {
-      console.error(e);
+    // Methods
+    async function updatePost(updatedPost) {
+        try {
+            await PostsApi.updatePost(updatedPost, post.id);
+        } catch (e) {
+            console.error(e);
+        }
     }
-  }
 
-  async function createComment(postId, commentData) {
-    try {
-      const response = await CommentsApi.createComment(postId, commentData);
-      const comment = response.data;
-      const newComments = comments.concat(comment);
-      setComments(newComments);
-    } catch (e) {
-      console.error(e);
+    async function createComment(postId, commentData) {
+        try {
+            const response = await CommentsApi.createComment(
+                postId,
+                commentData
+            );
+            const comment = response.data;
+            const newComments = comments.concat(comment);
+            setComments(newComments);
+        } catch (e) {
+            console.error(e);
+        }
     }
-  }
 
-  async function deleteComment(postId, commentToDelete) {
-    try {
-      await CommentsApi.deleteComment(postId, commentToDelete.id);
-      const newComments = comments.filter(
-        (comment) => comment.id !== commentToDelete.id
-      );
-      setComments(newComments);
-    } catch (e) {
-      console.error(e);
+    async function deleteComment(postId, commentToDelete) {
+        try {
+            await CommentsApi.deleteComment(postId, commentToDelete.id);
+            const newComments = comments.filter(
+                (comment) => comment.id !== commentToDelete.id
+            );
+            setComments(newComments);
+        } catch (e) {
+            console.error(e);
+        }
     }
-  }
 
-  function likeAction() {
-    if (checkForLikedUser()) {
-      removeLike();
-    } else {
-      addLike();
+    function likeAction() {
+        if (checkForLikedUser()) {
+            removeLike();
+        } else {
+            addLike();
+        }
+        window.location.reload();
     }
-    window.location.reload();
-  }
 
-  async function addLike() {
-    try {
-      await PostLikeApi.addLike(postId);
-    } catch (e) {
-      console.error(e);
+    async function addLike() {
+        try {
+            await PostLikeApi.addLike(postId);
+        } catch (e) {
+            console.error(e);
+        }
     }
-  }
 
-  async function removeLike() {
-    try {
-      await PostLikeApi.removeLike(postId);
-    } catch (e) {
-      console.error(e);
+    async function removeLike() {
+        try {
+            await PostLikeApi.removeLike(postId);
+        } catch (e) {
+            console.error(e);
+        }
     }
-  }
 
-  function checkUserEmail() {
-    return postCreatorEmail === currentUser.email;
-  }
+    function checkUserEmail() {
+        return postCreatorEmail === currentUser.email;
+    }
 
-  function checkForLikedUser() {
-    const likedEmail = listOfLikedUsers.find(
-      (user) => user.email === currentUser.email
-    );
-    return likedEmail != null;
-  }
+    function checkForLikedUser() {
+        const likedEmail = listOfLikedUsers.find(
+            (user) => user.email === currentUser.email
+        );
+        return likedEmail != null;
+    }
 
-  useEffect(() => {
-    CommentsApi.getAllComments(postId)
-      .then(({ data }) => setComments(data))
-      .catch((err) => console.error(err));
-  }, [setComments, postId]);
+    useEffect(() => {
+        CommentsApi.getAllComments(postId)
+            .then(({ data }) => setComments(data))
+            .catch((err) => console.error(err));
+    }, [setComments, postId]);
 
-  useEffect(() => {
-    UserApi.getUser()
-      .then(({ data }) => {
-        setCurrentUser(data);
-      })
-      .catch((err) => console.error(err));
-  }, [setCurrentUser]);
+    useEffect(() => {
+        UserApi.getUser()
+            .then(({ data }) => {
+                setCurrentUser(data);
+            })
+            .catch((err) => console.error(err));
+    }, [setCurrentUser]);
 
-  return (
-    <div className="PostCard">
-      <div>
-        <span className="post-info">{postCreatorName} posted </span>
-        <span className="post-info">
-          <Moment fromNow>{post.createdTime}</Moment>
-        </span>
-        <p className="content-text">{post.contentText}</p>
-        <div className="like-counter">
-          <img className="icon-like" src={likeIconURL} alt="like(s)"/>
-          <span>  {post.listOfLikes.length}</span>
+    return (
+        <div className="PostCard">
+            <div>
+                <div className="postcard-header">
+                    <span className="post-info">
+                        {post.listOfLikes.length} {postCreatorName} posted{" "}
+                        <Moment fromNow>{post.createdTime}</Moment>
+                    </span>
+                    <div className="delete-edit-icons">
+                        {checkUserEmail() && (
+                            <span>
+                                <button
+                                    className="button-post-card"
+                                    onClick={onDeleteClick}
+                                >
+                                    <img
+                                        src={deleteURL}
+                                        alt="delete-icon"
+                                        className="delete-icon"
+                                    ></img>
+                                </button>
+                                <button
+                                    className="button-post-card"
+                                    id="edit-button"
+                                    onClick={() =>
+                                        toggleUpdatePost
+                                            ? setToggleUpdatePost(false)
+                                            : setToggleUpdatePost(true)
+                                    }
+                                >
+                                </button>
+                            </span>
+                        )}
+                    </div>
+                </div>
+                <p className="content-text">
+                    {!toggleUpdatePost ? (
+                        post.contentText
+                    ) : (
+                        <div>
+                            <span>
+                                <PostUpdateForm
+                                    onSubmit={(postData) =>
+                                        updatePost(postData)
+                                    }
+                                    post={post}
+                                />
+                            </span>
+                            <span></span>
+                        </div>
+                    )}
+                </p>
+                <div className="like-counter">
+                    <img
+                        className="icon-like"
+                        src={likeIconURL}
+                        alt="like(s)"
+                    />
+                </div>
+                <button onClick={likeAction} className="like-button">
+                    {checkForLikedUser() ? "Remove Like" : "Like"}
+                </button>
+                <CommentList
+                    postId={postId}
+                    comments={comments}
+                    currentUser={currentUser}
+                    onDelete={deleteComment}
+                />
+                <CommentForm post={post} onSubmit={createComment} />
+            </div>
         </div>
-        <button onClick={likeAction} className="like-button">
-          {checkForLikedUser() ? "Remove Like" : "Like"}
-        </button>
-        {checkUserEmail() && (
-          <div>
-            <button className="button-post-card" onClick={onDeleteClick}>Delete</button>
-            <button className="button-post-card"
-              onClick={() =>
-                toggleUpdatePost
-                  ? setToggleUpdatePost(false)
-                  : setToggleUpdatePost(true)
-              }
-            >
-              {toggleUpdatePost ? "Cancel Update" : "Update"}
-            </button>
-            {toggleUpdatePost && (
-              <PostUpdateForm
-                onSubmit={(postData) => updatePost(postData)}
-                post={post}
-              />
-            )}
-          </div>
-        )}
-        <CommentList
-          postId={postId}
-          comments={comments}
-          currentUser={currentUser}
-          onDelete={deleteComment}
-        />
-        <CommentForm post={post} onSubmit={createComment} />
-      </div>
-    </div>
-  );
+    );
 }
