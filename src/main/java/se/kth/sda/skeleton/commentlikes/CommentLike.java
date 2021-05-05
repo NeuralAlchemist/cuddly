4608059358a0c8a5461b8750d417ba44882fdbd1
Janko Radakovic <mailodjanka@gmail.com>
2021-05-05 13:32:37 +0200

    Adds CommentLike model

---
+++ src/main/java/se/kth/sda/skeleton/commentlikes/CommentLike.java
@@ -0,0 +1,56 @@
+package se.kth.sda.skeleton.commentlikes;
+
+import com.fasterxml.jackson.annotation.*;
+import se.kth.sda.skeleton.comments.Comment;
+import se.kth.sda.skeleton.posts.Post;
+import se.kth.sda.skeleton.user.User;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Represents a Like on a {@code Post} in a cuddly app as a JPA entity. This implementation of PostLike will autogenerate a primary key of type
+ * {@link Long} to indicate the current PostLike entity. A Post has a {@link String} contentText.
+ */
+@Entity
+public class CommentLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JoinColumn(nullable = false)
+    @NotNull
+    @JsonIgnore
+    private Comment likedComment;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"createdPosts"})
+    @JoinColumn(nullable = false)
+    private User likedCommentUser;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Comment getLikedComment() {
+        return likedComment;
+    }
+
+    public void setLikedComment(Comment likedComment) {
+        this.likedComment = likedComment;
+    }
+
+    public User getLikedCommentUser() {
+        return likedCommentUser;
+    }
+
+    public void setLikedCommentUser(User likedCommentUser) {
+        this.likedCommentUser = likedCommentUser;
+    }
+}
