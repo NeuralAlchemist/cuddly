c7d9ceef02ebaed8d6b59566ebd0617f57ff18be
deciocandido <decio.candido@hotmail.co.uk>
2021-05-05 11:57:38 +0200

    Set profile ID

--- src/main/java/se/kth/sda/skeleton/profile/Profile.java
+++ src/main/java/se/kth/sda/skeleton/profile/Profile.java
@@ -44,6 +44,10 @@ public class Profile {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getDisplayDescription() {
         return displayDescription;
     }
