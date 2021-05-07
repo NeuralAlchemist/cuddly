841c459c27e9f63540445a8a1465fb7edb34fbe8
En-Chi Liu <enchi.liu@gmail.com>
2021-05-07 14:50:40 +0200

    add endpoint to update user description

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -4,6 +4,10 @@ class UserApi {
   getUser() {
     return Api.get("/users");
   }
+
+  updateUserDescription(description) {
+    return Api.put("/users", description)
+  }
 }
 
 export default new UserApi();
