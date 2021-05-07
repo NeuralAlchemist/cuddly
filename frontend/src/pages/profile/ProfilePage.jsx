f1dfa02d3f47f29a32e166e75883aa5573365d8e
En-Chi Liu <enchi.liu@gmail.com>
2021-05-07 16:51:50 +0200

    import ProfileForm and ProfileCard

---
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -0,0 +1,16 @@
+// NPM Packages
+import React from "react";
+
+// Project files
+import ProfileForm from "../../components/profile/ProfileForm";
+import ProfileCard from "../../components/profile/ProfileCard";
+
+export default function ProfilePage() {
+  return (
+    <div className="ProfilePage">
+      <p>*PROFILE PAGE placeholder*</p>
+      <ProfileCard />
+      <ProfileForm />
+    </div>
+  );
+}
