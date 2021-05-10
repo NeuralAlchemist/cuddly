7d04b3a96f9cb5bcc86dfecb6c5ae10242f0e547
Yash Seeta <yashseeta94@gmail.com>
2021-05-10 20:14:31 +0200

    make comment card follow post card and add border rounded

--- frontend/src/components/comments/CommentCard.jsx
+++ frontend/src/components/comments/CommentCard.jsx
@@ -8,108 +8,121 @@ import CommentUpdateForm from "./CommentUpdateForm";
 import CommentLikeApi from "../../api/CommentLikeApi";
 
 export default function CommentCard({
-  postId,
-  comment,
-  onDeleteClick,
-  currentUser,
+    postId,
+    comment,
+    onDeleteClick,
+    currentUser,
 }) {
-  // Local State
-  const [toggle, setToggle] = useState(false);
+    // Local State
+    const [toggle, setToggle] = useState(false);
 
-  // Constants
-  const commentCreatorName = comment.relatedCommentUser.name;
-  const commentCreatorEmail = comment.relatedCommentUser.email;
-  const listOfCommentLikedUsers = comment.listOfCommentLikes.map(
-    (commentLike) => commentLike.likedCommentUser
-  );
-
-  // Methods
-  const handleDelete = () => {
-    // Invoke the passed in event callback
-    onDeleteClick(postId, comment);
-  };
-
-  async function updateComment(updatedComment) {
-    try {
-      await CommentsApi.updateComment(postId, comment.id, updatedComment);
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  async function addCommentLike() {
-    try {
-      await CommentLikeApi.addCommentLike(comment.id);
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  async function removeCommentLike() {
-    try {
-      await CommentLikeApi.removeCommentLike(comment.id);
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  function commentLikeAction() {
-    if (checkForCommentLikeUser()) {
-      removeCommentLike();
-    } else {
-      addCommentLike();
-    }
-    window.location.reload();
-  }
-
-  function checkCommentUserEmail() {
-    return commentCreatorEmail === currentUser.email;
-  }
-
-  function checkForCommentLikeUser() {
-    const likeUserEmail = listOfCommentLikedUsers.find(
-      (user) => user.email === currentUser.email
+    // Constants
+    const commentCreatorName = comment.relatedCommentUser.name;
+    const commentCreatorEmail = comment.relatedCommentUser.email;
+    const listOfCommentLikedUsers = comment.listOfCommentLikes.map(
+        (commentLike) => commentLike.likedCommentUser
     );
-    return likeUserEmail != null;
-  }
 
-  return (
-      <div className="comment-card">
-          <span>
-              {commentCreatorName}{" "}
-              <Moment className="time-lapse" fromNow>
-                  {comment.createdTime}
-              </Moment>
-          </span>
-          <div className="word-wrap comment-content">
-              {comment.contentText}{" "}
-          </div>
-          <button
-              onClick={commentLikeAction}
-              className={`like-button button-post-card ${
-                  checkForCommentLikeUser() ? "liked" : "not-liked"
-              }`}
-          ></button>
-          <span className="like-counter"> {comment.listOfCommentLikes.length}</span>
+    // Methods
+    const handleDelete = () => {
+        // Invoke the passed in event callback
+        onDeleteClick(postId, comment);
+    };
 
-          {checkCommentUserEmail() && (
-              <div>
-                  <button onClick={handleDelete}>Delete</button>
-                  <button
-                      onClick={() =>
-                          toggle ? setToggle(false) : setToggle(true)
-                      }
-                  >
-                      {toggle ? "Cancel Update" : "Update"}
-                  </button>
-                  {toggle && (
-                      <CommentUpdateForm
-                          onSubmit={(commentData) => updateComment(commentData)}
-                          comment={comment}
-                      />
-                  )}
-              </div>
-          )}
-      </div>
-  );
+    async function updateComment(updatedComment) {
+        try {
+            await CommentsApi.updateComment(postId, comment.id, updatedComment);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async function addCommentLike() {
+        try {
+            await CommentLikeApi.addCommentLike(comment.id);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async function removeCommentLike() {
+        try {
+            await CommentLikeApi.removeCommentLike(comment.id);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    function commentLikeAction() {
+        if (checkForCommentLikeUser()) {
+            removeCommentLike();
+        } else {
+            addCommentLike();
+        }
+        window.location.reload();
+    }
+
+    function checkCommentUserEmail() {
+        return commentCreatorEmail === currentUser.email;
+    }
+
+    function checkForCommentLikeUser() {
+        const likeUserEmail = listOfCommentLikedUsers.find(
+            (user) => user.email === currentUser.email
+        );
+        return likeUserEmail != null;
+    }
+
+    return (
+        <div className="comment-card">
+            <div className="postcard-header">
+                <div className="post-info">{commentCreatorName}</div>
+                <div className="delete-edit-icons">
+                    {checkCommentUserEmail() && (
+                        <span>
+                            <button
+                                className="button-post-card delete-icon"
+                                onClick={handleDelete}
+                            />
+                            <button
+                                className={`button-post-card ${
+                                    toggle ? " cancel" : " active"
+                                }`}
+                                onClick={() =>
+                                    toggle ? setToggle(false) : setToggle(true)
+                                }
+                            />
+                        </span>
+                    )}
+                </div>
+                <Moment className="time-lapse" fromNow>
+                    {comment.createdTime}
+                </Moment>
+            </div>
+            <div className="word-wrap comment-content">
+                {!toggle ? (
+                    comment.contentText
+                ) : (
+                    <div>
+                        <CommentUpdateForm
+                            onSubmit={(commentData) =>
+                                updateComment(commentData)
+                            }
+                            comment={comment}
+                        />
+                    </div>
+                )}
+            </div>
+            <button
+                onClick={commentLikeAction}
+                className={`like-button button-post-card ${
+                    checkForCommentLikeUser() ? "liked" : "not-liked"
+                }`}
+            ></button>
+            <span className="like-counter">
+                {" "}
+                {comment.listOfCommentLikes.length}
+            </span>
+        </div>
+    );
 }
