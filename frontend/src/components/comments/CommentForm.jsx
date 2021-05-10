4ca1a69ad6a2caa29508983d469cee3b12f513f1
Esther Chee <pnchee@gmail.com>
2021-05-10 18:09:11 +0200

    creat CommentForm.jsx and commentform.css

--- frontend/src/components/comments/CommentForm.jsx
+++ frontend/src/components/comments/CommentForm.jsx
@@ -1,10 +1,9 @@
 // NPM Packages
-import React from "react";
-
+import React from 'react';
 
 export default function CommentForm({ post, onSubmit }) {
   // Local State
-  const [commentContentText, setCommentContentText] = React.useState("");
+  const [commentContentText, setCommentContentText] = React.useState('');
 
   // Methods
   const handleSubmit = () => {
@@ -12,20 +11,24 @@ export default function CommentForm({ post, onSubmit }) {
     onSubmit(post.id, { contentText: commentContentText });
 
     // Clear the input field
-      setCommentContentText("");
+    setCommentContentText('');
   };
 
   return (
-    <form className="CommentForm">
-        <div>
-            <textarea
-            value={commentContentText}
-            onChange={(e) => setCommentContentText(e.target.value)}
+    <form>
+      <div className="postform-field">
+        <textarea
+          className="postform-input"
+          placeholder="Comment"
+          value={commentContentText}
+          onChange={(e) => setCommentContentText(e.target.value)}
         />
-        </div>
-        <div>
-            <button onClick={handleSubmit}>Comment</button>
-        </div>
+      </div>
+      <div>
+        <button className="button-comment" onClick={handleSubmit}>
+          <span>Comment</span>
+        </button>
+      </div>
     </form>
   );
 }
--- frontend/src/components/posts/PostForm.jsx
+++ frontend/src/components/posts/PostForm.jsx
@@ -1,11 +1,15 @@
 // NPM Packages
-import React, { useState } from 'react';
+
+import React, { useState, useRef, useEffect } from 'react';
+import '../../styles/components/postform.css';
 
 export default function PostForm({ onSubmit }) {
   // Local State
   const [contentText, setContentText] = useState('');
   const postObject = require('../../assets/images/post.svg');
   const postURL = postObject;
+
+  const textArea = useRef(null);
 
   // Methods
   const handleSubmit = () => {
@@ -16,17 +20,35 @@ export default function PostForm({ onSubmit }) {
     setContentText('');
   };
 
+  useEffect(() => {
+    const originalHeight = '52px';
+    textArea.current.style.height = originalHeight;
+    // counter 4px of padding included in scrollHeight
+    const newHeight = textArea.current.scrollHeight - 4;
+    textArea.current.style.height = `${newHeight}px`;
+    if (newHeight > 200) {
+      textArea.current.style.overflowY = 'scroll';
+    } else {
+      textArea.current.style.overflowY = 'none';
+    }
+  }, [contentText]);
+
+  const onTextAreaValueChange = (value) => {
+    setContentText(value);
+  };
+
   return (
     <div className="postform-container">
       <form className="postform">
         <div>
           <div>
             <div className="postform-field">
-              <textarea 
+              <textarea
+                ref={textArea}
                 className="postform-input"
                 placeholder="What's on your mind?"
                 value={contentText}
-                onChange={(e) => setContentText(e.target.value)}
+                onChange={(e) => onTextAreaValueChange(e.target.value)}
               />
             </div>
 
