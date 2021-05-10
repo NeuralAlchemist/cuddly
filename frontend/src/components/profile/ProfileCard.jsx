fcd1c66c379eb04f3b441e0e1d2f008b9a274b47
En-Chi Liu <enchi.liu@gmail.com>
2021-05-10 11:59:17 +0200

    Add description placeholder

--- frontend/src/components/profile/ProfileCard.jsx
+++ frontend/src/components/profile/ProfileCard.jsx
@@ -4,28 +4,35 @@ import React, { useState, useEffect } from "react";
 // Project files
 import UserApi from "../../api/UserApi";
 
-export default function ProfileCard() {    
-    // Local state
-    const [currentUser, setCurrentUser] = useState({});  
+export default function ProfileCard() {
+  // Local state
+  const [currentUser, setCurrentUser] = useState({});
 
-    // Constants
-    // const response = UserApi.getUser().catch((err) => console.error(err));
-    // const user = response.data;  
-    useEffect(() => {
-        UserApi.getUser()
-          .then(({ data }) => {
-            setCurrentUser(data);
-          })
-          .catch((err) => console.error(err));
-      }, [setCurrentUser]);
+  // Constants
+  // const response = UserApi.getUser().catch((err) => console.error(err));
+  // const user = response.data;
+  useEffect(() => {
+    UserApi.getUser()
+      .then(({ data }) => {
+        setCurrentUser(data);
+      })
+      .catch((err) => console.error(err));
+  }, [setCurrentUser]);
 
-    //   function checkUserDescriptionIsNotNull() {
-    //       return currentUser.description != null;
-    //   }
+  //   function checkUserDescriptionIsNotNull() {
+  //       return currentUser.description != null;
+  //   }
 
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h2>About you</h2>
+      {/* {currentUser.description === null && (
+        <p>Why not add a description to your profile?</p>
+      )} */}
+      {/* {currentUser.description != null && (
+        <p>Description: {currentUser.description}</p>
+      )} */}
+      <p>Debug description: {currentUser.description}</p>
+    </div>
+  );
 }
