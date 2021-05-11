7d19ce3bbd48025fbaa4b4bd064927225513b072
Janko Radakovic <mailodjanka@gmail.com>
2021-05-11 13:44:41 +0200

    Adds Liked posts feed

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -6,9 +6,8 @@ import { useRecoilState } from "recoil";
 import ProfileCard from "../../components/profile/ProfileCard";
 import UserApi from "../../api/UserApi";
 import PostsApi from "../../api/PostsApi";
-import PostCard from "../../components/posts/PostCard"
+import PostCard from "../../components/posts/PostCard";
 import { postsState } from "../../state/postsData";
-
 
 export default function ProfilePage() {
   // Global state
@@ -16,7 +15,6 @@ export default function ProfilePage() {
 
   // Local state
   const [thisUser, setThisUser] = useState({});
-
 
   // Methods
   useEffect(() => {
@@ -46,7 +44,27 @@ export default function ProfilePage() {
 
   // Components
   const userPostCards = posts
-  .filter((post) => thisUser.id === post.relatedPostUser.id)
+    .filter((post) => thisUser.id === post.relatedPostUser.id)
+    .map((post) => (
+      <PostCard
+        key={post.id}
+        post={post}
+        currentUser={thisUser}
+        onDeleteClick={() => deletePost(post)}
+      />
+    ));
+
+  let userPostLikes = [];
+
+  for (let i = 0; i<posts.length; i++) {
+    for (let j = 0; j < posts[i].listOfLikes.length; j++) {
+      if (posts[i].listOfLikes[j].likedUser.id === thisUser.id ) {
+        userPostLikes.push(posts[i])
+      }
+    }
+  }
+
+  const userLikesPostCards = userPostLikes
   .map((post) => (
     <PostCard
       key={post.id}
@@ -56,36 +74,6 @@ export default function ProfilePage() {
     />
   ));
 
-  const userLikedPosts = thisUser.likedPosts;
-  console.log('userLikedPosts', userLikedPosts);
-
-  // const getLikedPostID = userLikedPosts
-  // .map((post) => (
-  //   posts.filter((post.id) => object.id === post.id)
-  // ));
-
-  // console.log('getLikedPosts', getLikedPosts);
-
-  // // Get listOfLikes array for each post in posts
-  // const userLikesArray = posts
-  // .map((post) => post.listOfLikes);
-
-  // console.log('userLikesArray', userLikesArray);
-
-  // // Check if the array is empty (no likes)
-
-  // const notEmptyArrays = userLikesArray.filter((array) => array.length !== 0);
-
-  // const extractLikedUserID = userLikesArray.length !== 0 ?  
-
-
-  // // If it's not empty, filter for likes by this user
-  // userLikesArray.filter((postLike) => thisUser.id === postLike.likedUser.id) : null;
-
-  // console.log('userLikesArray', extractLikedUserID);
-
-
-
   return (
     <div className="ProfilePage">
       <h1>Profile</h1>
@@ -93,6 +81,7 @@ export default function ProfilePage() {
       <p>my posts</p>
       {userPostCards}
       <p>Liked posts</p>
+      {userLikesPostCards}
     </div>
   );
 }
