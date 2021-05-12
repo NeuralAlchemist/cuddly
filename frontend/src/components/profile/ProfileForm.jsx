02acb2ab1a3dc9ab8f93573ce3ce1d9e0438d756
En-Chi Liu <enchi.liu@gmail.com>
2021-05-12 10:06:00 +0200

    implement ResponsiveTextArea in ProfileForm

--- frontend/src/components/profile/ProfileForm.jsx
+++ frontend/src/components/profile/ProfileForm.jsx
@@ -1,9 +1,13 @@
 // NPM Packages
 import React, { useState, useEffect } from "react";
 
+// Project Files
+import ResponsiveTextArea from "../ResponsiveTextArea";
+
 export default function ProfileForm({ desc, onSubmit }) {
   // Local state
   const [description, setDescription] = useState("");
+  const [length, setLength] = useState();
 
   // Methods
   const handleSubmit = () => {
@@ -16,16 +20,23 @@ export default function ProfileForm({ desc, onSubmit }) {
     setDescription(desc);
   }, [desc]);
 
+  const onFormContentChange = (value) => {
+    setDescription(value);
+    setLength(value.length);
+  };
+
   return (
-      <form className="ProfileForm">
-        <textarea
-          className="description-input"
-          onChange={(e) => setDescription(e.target.value)}
-          value={description}
-        />
-        <button className="button-update" onClick={handleSubmit}>
-          Update
-        </button>
-      </form>
+    <form className="ProfileForm">
+      <ResponsiveTextArea
+        placeholder="Update your profile description"
+        contentText={description}
+        onFormContentChange={onFormContentChange}
+        maxLength="1000"
+      />
+      <p className="length">{length == null ? 0 : length}/1000</p>
+      <button className="button-update" onClick={handleSubmit}>
+        Update
+      </button>
+    </form>
   );
 }
