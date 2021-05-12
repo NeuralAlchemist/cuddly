b6f70ec8d83ebf04936311fcc532da43dd816971
Janko Radakovic <mailodjanka@gmail.com>
2021-05-12 15:31:47 +0200

    Adds javadocs in UserService

--- src/main/java/se/kth/sda/skeleton/user/UserService.java
+++ src/main/java/se/kth/sda/skeleton/user/UserService.java
@@ -21,6 +21,12 @@ public class UserService {
         return userRepository.findByEmail(email);
     }
 
+    /**
+     * check if the accountType is valid
+     *
+     * @param user user who's accountType is checked
+     * @return true if accountType is valid, false otherwise
+     */
     public boolean isAccountTypeValid(User user) {
 
         return user.getAccountType().equals("human") ||
@@ -29,6 +35,12 @@ public class UserService {
                 user.getAccountType().equals("caretaker");
     }
 
+    /**
+     * register a user
+     *
+     * @param user user to be registered
+     * save a user in the userRepository
+     */
     public void register(User user) {
         String encryptedPass = passwordEncoder.encode(user.getPassword());
         String accountType = user.getAccountType();
