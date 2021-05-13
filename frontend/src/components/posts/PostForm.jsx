5479fa419265c23178de535b34bbc82d5597b0ac
Esther Chee <pnchee@gmail.com>
2021-05-13 11:30:01 +0200

    remove crept

--- frontend/src/components/posts/PostForm.jsx
+++ frontend/src/components/posts/PostForm.jsx
@@ -5,21 +5,21 @@ import React, { useState } from 'react';
 import ResponsiveTextArea from '../ResponsiveTextArea';
 
 // Project Files
-import PostsApi from "../../api/PostsApi";
+import PostsApi from '../../api/PostsApi';
 
 export default function PostForm({ onSubmit, onSubmitMedia }) {
   // Local State
   const [contentText, setContentText] = useState('');
   const [length, setLength] = useState();
-  const postURL = require('../../assets/images/post.svg');
   const [contentFile, setContentFile] = useState();
   const [isFilePicked, setIsFilePicked] = useState(false);
-  
+  const postURL = require('../../assets/images/post.svg');
+
   // Methods
   const handleSubmit = (event) => {
     if (isFilePicked) {
       event.preventDefault();
-      console.log(contentFile)
+      console.log(contentFile);
       onSubmitMedia({ contentFile, contentText });
       setContentFile(null);
       setIsFilePicked(false);
@@ -27,16 +27,15 @@ export default function PostForm({ onSubmit, onSubmitMedia }) {
       // Invoke the passed in event callback
       onSubmit({ contentText: contentText });
       // Clear the input field
-      setContentText("");
+      setContentText('');
     }
-
   };
 
   const onFormContentChange = (value) => {
     setContentText(value);
     setLength(value.length);
   };
-  
+
   const setFile = (event) => {
     setContentFile(event.target.files[0]);
     setIsFilePicked(true);
@@ -45,20 +44,20 @@ export default function PostForm({ onSubmit, onSubmitMedia }) {
   return (
     <div className="form-container">
       <form className="form">
-            <div className="form-field">
-              <ResponsiveTextArea
-                placeholder="What's on your mind?"
-                contentText={contentText}
-                onFormContentChange={onFormContentChange}
-                maxLength="1000"
-              />
-            </div>
-            <p className="length">{length == null ? 0 : length}/1000git g</p>
-            <input type="file" onChange={setFile}/>
-            <button className="button-post" onClick={handleSubmit}>
-              <img className="post" src={postURL} alt="Post" />
-              <span>Post</span>
-            </button>
+        <div className="form-field">
+          <ResponsiveTextArea
+            placeholder="What's on your mind?"
+            contentText={contentText}
+            onFormContentChange={onFormContentChange}
+            maxLength="1000"
+          />
+        </div>
+        <p className="length">{length == null ? 0 : length}/1000</p>
+        <input type="file" onChange={setFile} />
+        <button className="button-post" onClick={handleSubmit}>
+          <img className="post" src={postURL} alt="Post" />
+          <span>Post</span>
+        </button>
       </form>
     </div>
   );
