d23044c08a44aac0cbb2f74f6c3d87a347c4c2d2
Esther Chee <pnchee@gmail.com>
2021-05-14 23:26:32 +0200

    style commentlist-heading

--- frontend/src/components/comments/CommentList.jsx
+++ frontend/src/components/comments/CommentList.jsx
@@ -1,16 +1,32 @@
 // NPM Packages
-import React from "react";
+import React from 'react';
 
 // Project files
-import CommentCard from "./CommentCard";
+import CommentCard from './CommentCard';
 
-export default function CommentList({ postId, comments, onDelete, currentUser }) {
+export default function CommentList({
+  postId,
+  comments,
+  onDelete,
+  currentUser,
+}) {
   // Components
   const CommentCardArray = comments.map((comment) => (
-    <CommentCard key={comment.id} postId={postId} comment={comment} currentUser={currentUser} onDeleteClick={onDelete} />
+    <CommentCard
+      key={comment.id}
+      postId={postId}
+      comment={comment}
+      currentUser={currentUser}
+      onDeleteClick={onDelete}
+    />
   ));
 
-  return <details className="CommentList"> 
-  <summary>Click to show all comments</summary>
-  {CommentCardArray}</details>;
+  return (
+    <details className="CommentList">
+      <summary className="commentlist-heading">
+        Click to show all comments
+      </summary>
+      {CommentCardArray}
+    </details>
+  );
 }
