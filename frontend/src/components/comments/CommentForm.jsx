67fdf74994f4532e13b008a5134b3263b94cd005
Yash Seeta <yashseeta94@gmail.com>
2021-05-15 12:01:06 +0200

    Multiple Changes
    add Formfooter component
    add checks for selected file before setting contentFile
    add onSubmitMedia into handleSubmit

--- frontend/src/components/comments/CommentForm.jsx
+++ frontend/src/components/comments/CommentForm.jsx
@@ -1,24 +1,48 @@
 // NPM Packages
-import React, { useState } from 'react';
-import ResponsiveTextArea from '../ResponsiveTextArea';
+import React, { useState } from "react";
+import ResponsiveTextArea from "../ResponsiveTextArea";
+import FormFooter from "../FormFooter";
+import getFileSizeInMB from "../../functions/getFileSizeInMB";
 
-export default function CommentForm({ post, onSubmit }) {
+export default function CommentForm({ post, onSubmit, onSubmitMedia }) {
   // Local State
-  const [commentContentText, setCommentContentText] = useState('');
+  const [contentText, setContentText] = useState("");
   const [length, setLength] = useState();
-  const commentURL = require('../../assets/images/comment.svg');;
-
+  const commentURL = require("../../assets/images/comment.svg");
+  const [contentFile, setContentFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
   // Methods
-  const handleSubmit = () => {
-    // Invoke the passed in event callback
-    onSubmit(post.id, { contentText: commentContentText });
-
+  const handleSubmit = (event) => {
+    if (isFilePicked) {
+      event.preventDefault();
+      console.log("Make API call to comment upload image");
+      onSubmitMedia(post.id, {contentFile, contentText});
+      setContentFile(null);
+      setIsFilePicked(false);
+    } else {
+      // Invoke the passed in event callback
+      onSubmit(post.id, { contentText: contentText });
+    }
     // Clear the input field
-    setCommentContentText('');
+    setContentText("");
+  };
+
+  const setFile = (event) => {
+    setIsFilePicked(false);
+    const file = event.target.files[0];
+    if (getFileSizeInMB(file.size) > 10) {
+      alert("Files larger than 10MB are not allowed!");
+    } else if (file.size === 0) {
+      alert("Empty files are not allowed!");
+    } else {
+      setContentFile(file);
+      console.log(contentFile);
+      setIsFilePicked(true);
+    }
   };
 
   const onFormContentChange = (value) => {
-    setCommentContentText(value);
+    setContentText(value);
     setLength(value.length);
   };
 
@@ -27,18 +51,19 @@ export default function CommentForm({ post, onSubmit }) {
       <div className="form-field">
         <ResponsiveTextArea
           placeholder="Write your comment here."
-          contentText={commentContentText}
+          contentText={contentText}
           onFormContentChange={onFormContentChange}
           maxLength="500"
         />
       </div>
-      <div>
-        <p className="length">{length == null ? 0 : length}/500</p>
-        <button className="button-comment" onClick={handleSubmit}>
-          <img className="comment" src={commentURL} alt="Commment" />
-          <span>Comment</span>
-        </button>
-      </div>
+      <FormFooter
+        isFilePicked={isFilePicked}
+        contentFile={contentFile}
+        length={length}
+        setFile={setFile}
+        handleSubmit={handleSubmit}
+        buttonText="Comment"
+      />
     </form>
   );
 }
