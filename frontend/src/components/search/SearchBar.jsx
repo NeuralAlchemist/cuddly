1ad3f6c7f952c5be7f5b80fe45f2cf060ff739c1
deciocandido <decio.candido@hotmail.co.uk>
2021-05-15 21:11:04 +0200

    Search Bar search by Id not textname

--- frontend/src/components/search/SearchBar.jsx
+++ frontend/src/components/search/SearchBar.jsx
@@ -1,26 +1,34 @@
 // NPM Packages
-import Reach from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import UserApi from "../../api/UserApi";
 
 export default function SearchBar(){
     // Local State
     const [query, setQuery] = useState("");
+    const [users, setUsers] = useState([]);
 
     // Constants
     const history = useHistory();
 
+    useEffect(() => {
+      UserApi.getUserById()
+      .then(({ data }) => {
+        setUsers(data);
+      })
+      .catch((err) => console.error(err));
+    }, [setUsers]);
+
     // Methods
     function searchUser(event) {
         event.preventDefault();
-        history.push(`/results/${query}`);
+        //Find out the user by the Id
+        history.push(`/profile/${query}`);
     }
 
     return (
         <div>
           <form onSubmit={searchUser} className="SearchUser">
-            <label htmlFor="sender-search" className="search-label" id="search-label">
-              Search by user's name
-            </label>
             <div className="search-input">
               <input
                 type="text"
