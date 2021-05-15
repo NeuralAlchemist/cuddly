dc0b5a0431ebbaa5cf632faba2a78f2bd8c53b25
deciocandido <decio.candido@hotmail.co.uk>
2021-05-15 17:29:40 +0200

    Search Card components started

---
+++ frontend/src/components/search/SearchCard.jsx
@@ -0,0 +1,8 @@
+// NPM package
+import { useRecoilValue } from "recoil";
+import { Link } from "react-router-dom";
+
+// Project files
+import UserApi from "../../api/UserApi";
+import Profile from "../../components/profile/Profile";
+
