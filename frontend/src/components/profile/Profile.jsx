3be4d3460b495ea0665cb2dbc5a18300ef17aa1f
En-Chi Liu <enchi.liu@gmail.com>
2021-05-15 13:28:27 +0200

    Update prompt text, add posts link and style link

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -4,8 +4,12 @@ import React from "react";
 // Project files
 import ProfileCard from "./ProfileCard";
 
-export default function Profile({thisUser, userPostCards, userLikesPostCards, isLoggedInUser}) {
-
+export default function Profile({
+  thisUser,
+  userPostCards,
+  userLikesPostCards,
+  isLoggedInUser,
+}) {
   // Constants
   const hasCreatedPosts = userPostCards.length > 0;
   const hasLikedPosts = userLikesPostCards.length > 0;
@@ -13,14 +17,30 @@ export default function Profile({thisUser, userPostCards, userLikesPostCards, is
   return (
     <div className="main-container-item ProfilePage">
       <h1 className="page-name">Profile</h1>
-      <ProfileCard thisUser={thisUser} isLoggedInUser={isLoggedInUser}/>
+      <ProfileCard thisUser={thisUser} isLoggedInUser={isLoggedInUser} />
       <div className="user-feed">
         <h3>Posts</h3>
         {hasCreatedPosts && userPostCards}
-        {!hasCreatedPosts && <p className="prompt">You haven't created any posts yet</p>}
+        {!hasCreatedPosts && (
+          <p>
+            Create{" "}
+            <a href="/posts" className="post-link">
+              posts
+            </a>{" "}
+            to see them here
+          </p>
+        )}
         <h3>Liked posts</h3>
         {hasLikedPosts && userLikesPostCards}
-        {!hasLikedPosts && <p className="prompt">You haven't liked any posts yet</p>}
+        {!hasLikedPosts && (
+          <p>
+            Like{" "}
+            <a href="/posts" className="post-link">
+              posts
+            </a>{" "}
+            to see them here
+          </p>
+        )}
       </div>
     </div>
   );
