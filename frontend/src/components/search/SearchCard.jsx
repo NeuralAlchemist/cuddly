d153f481cf789459c56c323c69a93ef33b2e20e2
deciocandido <decio.candido@hotmail.co.uk>
2021-05-15 21:11:20 +0200

    uncompleted SearchCard

--- frontend/src/components/search/SearchCard.jsx
+++ frontend/src/components/search/SearchCard.jsx
@@ -3,6 +3,17 @@ import { useRecoilValue } from "recoil";
 import { Link } from "react-router-dom";
 
 // Project files
+import { allUsers } from "../../state/usersData";
 import UserApi from "../../api/UserApi";
 import Profile from "../../components/profile/Profile";
 
+export default function SearchCard({ match }){
+    // Global state
+    const profile = useRecoilValue(allUsers);
+
+    // Constants
+    const query = match.params.query.toUpperCase();
+    const filteredResults = profile.filter((item) =>
+        item.sender.toUpperCase().match(query)
+    );
+}
