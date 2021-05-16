0c1e9205bb647eef9369a3eaf1f045f35a4d3c6c
Yash Seeta <yashseeta94@gmail.com>
2021-05-16 13:58:26 +0200

    revert removing forced reload

--- frontend/src/pages/posts/PostsPage.jsx
+++ frontend/src/pages/posts/PostsPage.jsx
@@ -31,16 +31,12 @@ export default function PostsPage() {
       console.log(imageFile);
       formData.append("file", imageFile.contentFile);
       formData.append("text", imageFile.contentText);
-      const response = await PostsApi.createImagePost(formData);
-      const post = response.data;
-      const newPosts = posts.concat(post);
-      const currPost = newPosts.pop();
-      newPosts.unshift(currPost);
+      await PostsApi.createImagePost(formData);
       await new Promise((r) => setTimeout(r, 1000));
-      setPosts(newPosts);
     } catch (e) {
       console.error(e);
     }
+    window.location.reload();
   }
 
   async function deletePost(post) {
