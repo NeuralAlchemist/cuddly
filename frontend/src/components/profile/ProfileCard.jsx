d71231a4f263e3f716d981ec408245bfc45c796f
En-Chi Liu <enchi.liu@gmail.com>
2021-05-17 23:17:21 +0200

    remove empty line

--- frontend/src/components/profile/ProfileCard.jsx
+++ frontend/src/components/profile/ProfileCard.jsx
@@ -88,7 +88,6 @@ export default function ProfileCard({ thisUser, isLoggedInUser }) {
                 >
                   Choose image
                 </label>
-
                 <input
                   hidden
                   id="profile-image-input"
