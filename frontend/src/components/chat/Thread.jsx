943b3b5b1889154fb65f09c5ae238ec1476471ec
Esther Chee <pnchee@gmail.com>
2021-05-17 11:49:11 +0200

    create chat component on FE

---
+++ frontend/src/components/chat/Thread.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Thread({ thread, setMessageBox }) {
+  const senderMail = window.sessionStorage.getItem('userEmail');
+  const receiverEmail = senderMail === thread.p1Email ? thread.p2Email : thread.p1Email;
+  const receiverMessage = thread.receiverMessage;
+
+  const clickHandler = () => {
+    setMessageBox({ threadId: thread.id, thread: thread });
+  };
+  const lastMessage = thread.thread.slice(-1)[0];
+  const lastDate = lastMessage === undefined ? null : lastMessage.date;
+
+  return (
+    <div className="chat_people">
+      <div className="chat_img">
+        {' '}
+        <img src="/images/sender.jpeg" alt="name" />{' '}
+      </div>
+      <div className="chat_ib">
+        <h5>
+          <Link to="/chat" onClick={clickHandler}>
+            {receiverEmail}
+          </Link>
+          <span className="chat_date">{lastDate}</span>
+        </h5>
+        <p>{receiverMessage}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Thread;
---
+++ frontend/src/components/chat/ThreadPage.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import ChatApi from '../../api/ChatApi';
+import ChatPage from './ChatPage';
+import Thread from './Thread';
+
+// Todo: We should still filter out message threads based on user.
+// Also, sometimes a second thread is generated even though one between the participants exist.
+// We should revisit the backend to sort this issue.
+
+function ThreadPage() {
+  const [threads, setThreads] = useState([]);
+  const { id } = useParams();
+  const { state } = useLocation();
+  const locationState =
+    state === null || state === undefined ? '' : state.thread;
+  const [messageBox, setMessageBox] = useState({
+    threadId: id,
+    thread: locationState,
+  });
+
+  useEffect(() => {
+    const getThreads = async () => {
+      const response = await ChatApi.getAllThread();
+      setThreads(response.data);
+    };
+    getThreads();
+  }, []);
+
+  const listOfThreads = threads.map((thread) => (
+    <Thread key={thread.id} setMessageBox={setMessageBox} thread={thread} />
+  ));
+  return (
+    <div>
+      <div className="messaging">
+        <div className="inbox_msg">
+          <div className="inbox_people">
+            <div className="headind_srch">
+              <div className="recent_heading">
+                <h4>Recent</h4>
+              </div>
+              <div className="srch_bar">
+                <div className="stylish-input-group">
+                  <input
+                    type="text"
+                    className="search-bar"
+                    placeholder="Search"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="inbox_chat scroll">
+              <div className="chat_list active_chat">
+                {threads === [] ? 'loading...' : listOfThreads}
+              </div>
+            </div>
+          </div>
+          <div className="mesgs">
+            {messageBox.thread === '' ? null : (
+              <ChatPage id={messageBox.threadId} thread={messageBox.thread} />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ThreadPage;
