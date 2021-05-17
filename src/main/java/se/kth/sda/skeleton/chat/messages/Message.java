0771b44777f94cefa150770a761c652c880b9c52
Esther Chee <pnchee@gmail.com>
2021-05-17 11:31:08 +0200

    add message model

---
+++ src/main/java/se/kth/sda/skeleton/chat/messages/Message.java
@@ -0,0 +1,93 @@
+package se.kth.sda.skeleton.chat.messages;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import se.kth.sda.skeleton.chat.threads.MessageThread;
+
+import javax.persistence.*;
+
+@Table(name = "messages")
+@Entity
+public class Message {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "message_generator")
+    @SequenceGenerator(name = "message_generator", sequenceName = "message_seq")
+    private Long id;
+
+    @Column(name = "sender_email")
+    private String senderEmail;
+
+    @Column(name = "receiver_email")
+    private String receiverEmail;
+
+    @ManyToOne
+    private MessageThread thread;
+
+    @Column(name = "message_body", columnDefinition = "TEXT")
+    private String messageBody;
+
+    @Column(name = "date")
+    private String date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Message(Long id, String senderEmail, String receiverEmail, MessageThread thread, String messageBody, String date) {
+        this.id = id;
+        this.senderEmail = senderEmail;
+        this.receiverEmail = receiverEmail;
+        this.thread = thread;
+        this.messageBody = messageBody;
+        this.date = date;
+    }
+
+    public Message() {
+    }
+
+    public String getMessageBody() {
+        return messageBody;
+    }
+
+    public void setMessageBody(String messageBody) {
+        this.messageBody = messageBody;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @JsonBackReference
+    public MessageThread getThread() {
+        return thread;
+    }
+
+    public void setThread(MessageThread thread) {
+        this.thread = thread;
+    }
+
+    public String getSenderEmail() {
+        return senderEmail;
+    }
+
+    public void setSenderEmail(String senderEmail) {
+        this.senderEmail = senderEmail;
+    }
+
+    public String getReceiverEmail() {
+        return receiverEmail;
+    }
+
+    public void setReceiverEmail(String receiverEmail) {
+        this.receiverEmail = receiverEmail;
+    }
+}
---
+++ src/main/java/se/kth/sda/skeleton/chat/messages/MessageController.java
@@ -0,0 +1,57 @@
+package se.kth.sda.skeleton.chat.messages;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import se.kth.sda.skeleton.auth.AuthService;
+import se.kth.sda.skeleton.chat.threads.MessageThread;
+import se.kth.sda.skeleton.chat.threads.MessageThreadService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chat")
+public class MessageController {
+
+    private final MessageService messageService;
+    private final MessageThreadService threadService;
+    private final AuthService authService;
+
+    @Autowired
+    public MessageController(MessageService messageService, MessageThreadService threadService, AuthService authService) {
+        this.messageService = messageService;
+        this.threadService = threadService;
+        this.authService = authService;
+
+    }
+
+    @GetMapping("")
+    public List<MessageThread> getAll() {
+        return threadService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public MessageThread getThread(@PathVariable Long id) {
+        return threadService.getById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    // Refer to comment in MessageThreadRepository
+    @PostMapping("")
+    public MessageThread createThread(MessageThread newThread, @RequestParam(required = true) String receiverEmail) {
+        String senderEmail = authService.getLoggedInUserEmail();
+        MessageThread existing = threadService.findByEmails(senderEmail, receiverEmail);
+        if (existing != null) return existing;
+        newThread.setP1Email(senderEmail);
+        newThread.setP2Email(receiverEmail);
+        return threadService.create(newThread);
+    }
+
+    @PostMapping("/{id}")
+    public Message createMessage(@RequestBody Message newMessage, @RequestParam String receiverEmail) {
+        String senderEmail = authService.getLoggedInUserEmail();
+        newMessage.setReceiverEmail(receiverEmail);
+        newMessage.setSenderEmail(senderEmail);
+        return messageService.create(newMessage);
+    }
+}
