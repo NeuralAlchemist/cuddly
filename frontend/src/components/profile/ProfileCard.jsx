81eb07192e33486d89a2ffa06c4108cc2fbdcb16
deciocandido <decio.candido@hotmail.co.uk>
2021-05-17 18:43:31 +0200

    Profile with selected image

--- frontend/src/components/profile/ProfileCard.jsx
+++ frontend/src/components/profile/ProfileCard.jsx
@@ -4,13 +4,18 @@ import React, { useState } from "react";
 // Project files
 import UserApi from "../../api/UserApi";
 import ProfileForm from "../../components/profile/ProfileForm";
+import getFileSizeInMB from "../../functions/getFileSizeInMB";
 
 export default function ProfileCard({ thisUser, isLoggedInUser }) {
   // Local state
   const [toggleEdit, setToggleEdit] = useState(false);
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFileSelected, setIsFileSelected] = useState(false);
 
   // Constants
   const userURL = require("../../assets/images/user.svg");
+  const hasImage =
+    thisUser.mediaType == null ? false : thisUser.mediaType.includes("image");
 
   // Methods
   async function updateUser(updatedUser) {
@@ -21,12 +26,57 @@ export default function ProfileCard({ thisUser, isLoggedInUser }) {
     }
   }
 
+  async function uploadImage(event){
+    console.log("Sending");
+    event.preventDefault();
+    try{
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+      await UserApi.uploadImage(thisUser.id, formData);
+    } catch (e){
+      console.error(e);
+    }
+    window.location.reload();
+  }
+
+  const setFile = (event) => {
+    setIsFileSelected(false);
+    const file = event.target.files[0];
+    if (getFileSizeInMB(file.size) > 10) {
+      alert("Files larger than 10MB are not allowed!");
+    } else if (file.size === 0) {
+      alert("Empty files are not allowed!");
+    } else {
+      setSelectedFile(file);
+      setIsFileSelected(true);
+    }
+  };
+
   return (
     <div className="ProfileCard">
       <div className="user-fields">
         <div className="name-pair">
-          <img className="user-avatar" src={userURL} alt="User" />
+          {!hasImage && (
+          <img
+            className="user-avatar"
+            src={userURL}
+            height="100%"
+            width="100%"
+            alt="User"
+          />
+        )}
+
+          {hasImage && (
+          <img
+            className="user-avatar"
+            src={`data:${thisUser.imageType};base64, ${thisUser.image}`}
+            height="100%"
+            width="100%"
+            alt="User"
+          />
+        )}
           <h2 className="name">{thisUser.name}</h2>
+          <input type="file" onChange={setFile}/><button onClick={uploadImage}>Upload</button>
         </div>
         {isLoggedInUser && (
           <button
