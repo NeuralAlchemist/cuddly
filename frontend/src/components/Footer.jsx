d999662996195e7871856521df9e36a7c78d3955
deciocandido <decio.candido@hotmail.co.uk>
2021-05-18 15:27:54 +0200

    removed the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,13 +4,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer className="footer">
-      <Link className="footer-link">
-        <h5>About us</h5>
-      </Link>
-
-      <Link className="footer-link">
-        <h5>Terms and conditions</h5>
-      </Link>
+  
     </footer>
   );
 }
